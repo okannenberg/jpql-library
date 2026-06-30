a54cbd7dfcb417ee7c7e1fe06bdc09cf59f1774e
Bruno Gustavo Kannenberg <brunokannenberg794@gmail.com>
2026-06-30 12:51:32 -0300

    Feat: Criada a classe Livro

--- src/main/java/com/jpql/biblioteca/model/Livro.java
+++ src/main/java/com/jpql/biblioteca/model/Livro.java
@@ -1,4 +1,45 @@
 package com.jpql.biblioteca.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "livro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private LocalDate dataPublicacao;
+
+    @Column(nullable = false)
+    private String categoria;
+
+    @ManyToOne
+    private Editora editora;
+
+    @ManyToMany
+    @JoinTable(name = "livro_autores")
+    private List<Autor> autores;
 }
